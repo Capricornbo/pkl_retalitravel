ee12602d3c58a38eb09fb879790421506f2632c1
Capricornbo <152087175+Capricornbo@users.noreply.github.com>
2024-10-23 15:04:33 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projek_pkl/loginscreen.dart';
-import 'package:projek_pkl/procedurepage.dart';
-import 'package:projek_pkl/splashscreen.dart';
-import 'package:projek_pkl/tugaspage.dart';
-import 'package:projek_pkl/kontentpage.dart';
-import 'package:projek_pkl/homepage.dart';
-import 'package:projek_pkl/loginpage.dart';
-import 'package:projek_pkl/profilepage.dart';
-
-import 'loginscreen.dart';
+import 'package:naskah/naskah_screen.dart'; // Mengimpor naskah_screen.dart
 
 void main() {
   runApp(MyApp());
@@ -18,7 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Splashscreen2(), // Tanpa .new()
+      title: 'Pilihan Naskah',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: NaskahScreen(),
     );
   }
 }
